1e26064583550af892bf44aa287c0890b7c85ea4
Alessandro Maclaine <almaclaine@gmail.com>
2023-05-07 13:15:22 -0500

    work

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,64 +1,39 @@
-import { tap, succeed, chain } from '@effect-ts/core/Effect';
-import * as T from '@effect-ts/core/Effect';
-import { makeRef } from '@effect-ts/core/Effect/Ref';
-import { pipe } from '@effect-ts/core/Function';
-import { tryCatch } from '@effect-ts/core/Effect/Utils';
+import * as RTE from 'fp-ts/ReaderTaskEither';
+import * as TE from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 
-export interface CacheItem<T> {
-  data: T;
-  timestamp: number;
+interface Logger {
+  log: (message: string, value: unknown) => void;
 }
 
-export const cache = new Map<string, CacheItem<unknown>>();
-
-export function fetchFromNetwork<T>(url: string) {
-  return tryCatch(
-    () => fetch(url).then((res) => res.json() as Promise<T>),
-    (e) => new Error(`Failed to fetch ${url}: ${String(e)}`)
-  );
+function logReaderTaskEither<A>(
+  message: string,
+  value: A
+): RTE.ReaderTaskEither<Logger, never, A> {
+  return (logger: Logger) => {
+    logger.log(message, value);
+    return TE.right(value);
+  };
 }
 
-// Add type parameter `K` for the key type
-export function getFromCache<T>(url: string, maxAge = 5 * 60 * 1000) {
-  // Use `Record<K, CacheItem<T>>` to define the type of the cache map
-  return pipe(
-    makeRef<CacheItem<T> | undefined>(
-      cache.get(url) as CacheItem<T> | undefined
-    ),
-    chain((ref) =>
-      pipe(
-        ref.get,
-        chain((cachedItem) =>
-          cachedItem && Date.now() - cachedItem.timestamp < maxAge
-            ? succeed(cachedItem.data)
-            : pipe(
-                fetchFromNetwork<T>(url),
-                tap((data) => {
-                  ref.set({
-                    ...cachedItem,
-                    data,
-                    timestamp: Date.now(),
-                  });
-                  // Update cache map with the new data and timestamp
-                  cache.set(url, { data, timestamp: Date.now() });
-                })
-              )
-        )
-      )
-    )
-  );
-}
+// Your async TaskEither functions
+const asyncFn1: TE.TaskEither<Error, number> = TE.right(42);
+const asyncFn2: (n: number) => TE.TaskEither<Error, string> = (n) =>
+  TE.right(`Result: ${n}`);
 
-// const main = T.gen(function* (_) {
-//   const url = 'https://jsonplaceholder.typicode.com/todos/1';
-//
-//   const result = yield* getFromCache(url);
-//   console.log('First fetch result:', result);
-//
-//   yield* T.sleep(2000); // 2 seconds delay
-//
-//   const result2 = yield* getFromCache(url);
-//   console.log('Second fetch result:', result2);
-// });
-//
-// T.run(main);
+const pipeline = pipe(
+  RTE.fromTaskEither(asyncFn1),
+  RTE.chain((value) => logReaderTaskEither('Initial value:', value)),
+  RTE.chain(asyncFn2),
+  RTE.chain((value) => logReaderTaskEither('Final value:', value))
+);
+
+// Provide the Logger instance
+const consoleLogger: Logger = {
+  log: (message: string, value: unknown) => console.log(message, value),
+};
+
+// Execute the pipeline
+RTE.run(pipeline, consoleLogger)().catch((error) =>
+  console.error('Error:', error.message)
+);
